51eb10645c18ee07a1949ca5a3070aa58117b270
Akash <akashbca9508@gmail.com>
2024-01-27 22:45:58 +0530

    second

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ t1.from("#body", {
     x: "-100%",
     opacity: 0,
     ease: "back.out",
-},"akash")
+},)
 t1.from(".container", {
     x: "-100%",
     opacity: 0,
@@ -18,22 +18,22 @@ t1.from("#text1", {
     x: "-100%",
     opacity: 0,
     ease: "back.out",
-},"akash")
+},)
 t1.from("#button", {
     y: "-100%",
     opacity: 0,
     ease: "back.out",
-},"akash")
+},)
 t1.from(".jokes-area", {
     x: "100%",
     opacity: 0,
     ease: "back.out",
-},"akash")
+},)
 t1.from("#button2", {
     y: "-100%",
     opacity: 0,
     ease: "back.out",
-},"akash")
+},)
 
 
 // animation end 
